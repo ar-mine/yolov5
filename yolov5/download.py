609422af4c65c6bbea841d5c1f9b5bdf1163e42f
934218777@qq.com <934218777@qq.com>
2022-12-20 09:06:49 +0800

    Add functions to list predefined models

--- yolov5/download.py
+++ yolov5/download.py
@@ -2,6 +2,7 @@ import gdown
 import sys
 import os
 import yaml
+import argparse
 
 ROOT = os.path.dirname(__file__)
 
@@ -13,19 +14,25 @@ def download(prefix, file_id, file_name):
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python download.py model_name")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name", help="The predefined model to be downloaded.")
+    parser.add_argument("-l", "--list", help="List all the models that can be downloaded.", action="store_true")
+    args = parser.parse_args()
+
     with open(os.path.join(ROOT, "model_ids.yaml"), 'r', encoding="utf-8") as f:
         file_data = f.read()
+        data = yaml.load(file_data, Loader=yaml.FullLoader)
+    key_list = list(data.keys())
 
-    # print(file_data)
-    # print("Type: ", type(file_data))
+    if args.list:
+        print("The supported models is" + "".join(key_list))
 
-    data = yaml.load(file_data, Loader=yaml.FullLoader)
-    if sys.argv[1] in data.keys():
-        contains = data[sys.argv[1]]
+    if args.name in key_list:
+        contains = data[args.name]
         download('data', contains['data']['link'], contains['data']['file'])
         download('models', contains['models']['link'], contains['models']['file'])
+    else:
+        print("Wrong model's name, the supported models is" + "".join(key_list))
 
 
 if __name__ == "__main__":
